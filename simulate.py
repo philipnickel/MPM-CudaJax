bf7b330b04e9d997e96d572ff857d110b9390fef
philipnickel7 <philipnickel7@gmail.com>
2026-06-02 12:21:03 +0000

    refactor: move get_particles into blocks/init.py

--- simulate.py
+++ simulate.py
@@ -6,13 +6,7 @@ from tqdm import tqdm
 import hydra
 from omegaconf import DictConfig
 
-
-def get_particles(n_particles, center, size):
-    """Sample n_particles uniformly in a box."""
-    start = np.array(center, dtype=np.float32) - np.array(size, dtype=np.float32) / 2
-    end = np.array(center, dtype=np.float32) + np.array(size, dtype=np.float32) / 2
-    rng = np.random.RandomState(42)
-    return (start + rng.rand(n_particles, 3).astype(np.float32) * (end - start)).astype(np.float32, copy=False)
+from mpm_jax.blocks.init import get_particles
 
 
 def visualize_frames(frames, export_path, center=[0.5, 0.5, 0.5],
